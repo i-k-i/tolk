c8c76bc820565a08100d5cffe93639a8d7622255
i-k-i <w5540554@gmail.com>
2014-09-22 17:11:43 +0400

    remember

--- projector/models.py
+++ projector/models.py
@@ -5,29 +5,38 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 from taggit.managers import TaggableManager
-# Create your models here.
 
+from datetime import datetime
 
 class Project(models.Model):
     name = models.CharField(max_length=200, default=str(timezone.now()))
-    startdate = models.DateTimeField(default=timezone.now())
-    deadline = models.DateTimeField(default=timezone.now()+10)
+    startdate = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
     author = models.ForeignKey(User)
     description = models.TextField(default='Just one more thing')
     tags = TaggableManager()
-    status =
-    comments =
-
+    status = models.CharField(max_length=50, default='dreams')
 
     def __str__(self):
         return self.name
 
-#class PrStatus(models):
-
 class Task(models.Model):
     name = models.CharField(max_length=200)
     startdate = models.DateTimeField(default=timezone.now())
+    status = models.CharField(max_length=50, default='dreams')
+    expected_time = models.TimeField('expected time')
+    real_time = models.TimeField()
+    workers = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.name
 
 class PrComments(models.Model):
-    author=
+    author = models.ForeignKey(User)
+    date = models.DateTimeField('date published', default = datetime.now)
+    project = models.ForeignKey(Project)
+
+class TaskComment(models.Model):
+    author = models.ForeignKey(User)
+    date = models.DateTimeField(default=timezone.now())
+    task = models.ForeignKey(Task)
