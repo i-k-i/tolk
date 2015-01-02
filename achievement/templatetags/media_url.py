2f24a596b8f99d4b9f1cb68dcc00bb4c070eb8f2
i-k-i <w5540554@gmail.com>
2015-01-03 02:35:06 +0400

    achivement_templatetags_removed

--- achievement/templatetags/media_url.py
+++
@@ -1,9 +0,0 @@
-from django.template import Library
-from django.conf import settings
-register = Library()
-
-@register.simple_tag(name='media')
-def media_url():
-    print settings.MEDIA_URL
-    print 22222222222
-    return settings.MEDIA_URL
